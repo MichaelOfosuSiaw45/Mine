ccb2ce5fa28a5cd56a20182dd36594cce1cdda43
MichaelOfosuSiaw45 <67522677+MichaelOfosuSiaw45@users.noreply.github.com>
2020-06-27 22:49:16 +0100

    Add files via upload

---
+++ EVEN OR ODD NUM CHECK.js
@@ -0,0 +1,10 @@
+function check() {
+    var a;
+    a = Number(document.getElementById("a").value);
+    if (a % 2 == 0) {
+        console.log("This is Even Number");
+    } 
+    else {
+        console.log("This is Odd Number");
+    }
+}
